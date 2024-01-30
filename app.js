9fb2b527c8931ab7dc67a98391510a4da2a4b454
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-01-30 16:24:09 -0300

    css and html added

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
+// uses
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+// Statics
+app.use('/public', express.static(path.join(__dirname, '/public')))
+
+
+// default route
 app.get('/', (req, res)=>{
-    res.send('Okay')
+    return res.sendFile(path.join(__dirname, '/public/views/index.html'))
 })
 
+
+// init server
 app.listen(8080, ()=>{
     console.log('server on')
 })
