f69addf1add345d78a97f9240c072dd5d720f957
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-01-31 23:15:09 -0300

    init the "in-memory database"

--- src/database/memoryDatabase.js
+++ src/database/memoryDatabase.js
@@ -1,5 +1,14 @@
-class db {
+class Database{
+    static infos = new Map()
+
+    constructor(){
+        this.id = 0;
+    }
+
+    updateId(){
+        this.id += 1;
+    }
 }
 
-module.exports = db;
+module.exports = Database;
 
